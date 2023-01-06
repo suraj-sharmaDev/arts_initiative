3bc1725c961c63ee34ed62ffbefbc4d7fdf867ba
Suraj Sharma <suraj.sharma@emvigotech.com>
2023-01-06 18:20:55 +0530

    Unnecessary try catch wrapper removed

--- src/lib/unlink.ts
+++ src/lib/unlink.ts
@@ -5,11 +5,6 @@ const baseDirectory = path.join(process.cwd(), "/public");
 
 const unlink = async (folderName: string) => {
   const uploadDirectory = path.join(baseDirectory, folderName);
-  try {
-    await fs.access(uploadDirectory);
-  } catch (error) {
-    throw error;
-  }
   return await fs.unlink(uploadDirectory);
 };
 
